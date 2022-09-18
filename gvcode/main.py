2873e7a9bb51fc89c0b13c86b5b7e616b45206d8
huangqimin001 <huangqimin001@ke.com>
2022-09-18 13:10:26 +0800

    :sparkles: Support generate random fill color

--- gvcode/main.py
+++ gvcode/main.py
@@ -10,7 +10,10 @@ from .compat import BytesIO, is_py2, range
 try:
     from PIL import Image, ImageDraw, ImageFilter, ImageFont
 except ImportError:
-    import Image, ImageDraw, ImageFilter, ImageFont
+    import Image
+    import ImageDraw
+    import ImageFilter
+    import ImageFont
 
 
 class GraphicVerificationCode(object):
@@ -22,28 +25,41 @@ class GraphicVerificationCode(object):
         self.FONT_DIR = os.path.join(self.SRC_DIR, 'fonts')
         self.FONT_FILE = os.path.join(self.FONT_DIR, 'simsun.ttc')
 
-    def generate(self, size=(120, 30), chars=None, format='PNG', mode='RGB', bg_color=(255, 255, 255), fg_color=(0, 0, 255), font_size=18, font_file=None, length=4, draw_lines=True, line_range=(1, 2), draw_points=True, point_chance=2):
+    def generate(self, size=(120, 30), chars=None, mode='RGB', bg_color=(255, 255, 255), fg_color=(0, 0, 255), font_size=18, font_file=None, length=4, draw_lines=True, line_range=(1, 2), draw_points=True, point_chance=2, random_color=True):
 
         """
-        @param size: 图片的大小，格式（宽，高），默认为(120, 30)
-        @param chars: 允许的字符集合，格式字符串
-        @param format: 图片保存的格式，默认为 PNG，可选的为 GIF，JPEG，TIFF，PNG
-        @param mode: 图片模式，默认为 RGB
-        @param bg_color: 背景颜色，默认为白色
-        @param fg_color: 前景色，验证码字符颜色，默认为蓝色 #0000FF
-        @param font_size: 验证码字体大小
-        @param font_file: 验证码字体，默认为 None
-        @param length: 验证码字符个数
-        @param draw_lines: 是否划干扰线
-        @param line_range: 干扰线的条数范围，格式元组，默认为 (1, 2)，只有 draw_lines 为 True 时有效
-        @param draw_points: 是否画干扰点
-        @param point_chance: 干扰点出现的概率，大小范围 [0, 100]，只有 draw_points 为 True 时有效
-        @return: [0]: PIL Image 实例
-        @return: [1]: 验证码图片中的字符串
+        @:param size: 图片的大小，格式（宽，高），默认为 (120, 30)
+        @:param chars: 允许的字符集合，格式字符串
+        @:param mode: 图片模式，默认为 RGB
+        @:param bg_color: 背景色，默认为 白色
+        @:param fg_color: 前景色，验证码字符颜色，默认为 蓝色 #0000FF，只有 random_color 为 False 时有效
+        @:param font_size: 验证码字体大小
+        @:param font_file: 验证码字体，默认为 None
+        @:param length: 验证码字符个数，默认为 4
+        @:param draw_lines: 是否划干扰线，默认为 是
+        @:param line_range: 干扰线的条数范围，格式元组，默认为 (1, 2)，只有 draw_lines 为 True 时有效
+        @:param draw_points: 是否画干扰点，默认为 是
+        @:param point_chance: 干扰点出现的概率，大小范围 [0, 100]，只有 draw_points 为 True 时有效
+        @:param random_color: 是否随机生成填充色，默认为 是
+        @:return: [0]: PIL Image 实例
+        @:return: [1]: 验证码图片中的字符串
         """
         width, height = size  # 宽， 高
         im = Image.new(mode, size, bg_color)  # 创建图形
         draw = ImageDraw.Draw(im)  # 创建画笔
+
+        def generate_random_color():
+            return random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
+
+        def generate_fill_color():
+            if not random_color:
+                return 0, 0, 0
+            return generate_random_color()
+
+        def generate_fg_color():
+            if not random_color:
+                return fg_color
+            return generate_random_color()
 
         def generate_chars():
             """ 生成给定长度的字符串，返回列表格式 """
@@ -55,7 +71,7 @@ class GraphicVerificationCode(object):
 
         def create_line():
             """ 绘制干扰线 """
-            draw.line([random_dot(), random_dot()], fill=(0, 0, 0))
+            draw.line([random_dot(), random_dot()], fill=generate_fill_color())
 
         def create_lines():
             """ 绘制干扰线 """
@@ -65,7 +81,7 @@ class GraphicVerificationCode(object):
         def create_points():
             """ 绘制干扰点 """
             chance = min(100, max(0, int(point_chance)))  # 大小限制在[0, 100]
-            [draw.point((w, h), fill=(0, 0, 0)) for w in range(width) for h in range(height) if random.randint(0, 100) < chance]
+            [draw.point((w, h), fill=generate_fill_color()) for w in range(width) for h in range(height) if random.randint(0, 100) < chance]
 
         def create_chars():
             """ 绘制验证码字符 """
@@ -75,7 +91,7 @@ class GraphicVerificationCode(object):
             font = ImageFont.truetype(font_file if os.path.exists(font_file or '') else self.FONT_FILE, font_size)
             _, _, font_width, font_height = font.getbbox(strs)
             xy = ((width - font_width) / 3, (height - font_height) / 3)
-            draw.text(xy, strs, font=font, fill=fg_color)
+            draw.text(xy, strs, font=font, fill=generate_fg_color())
             return ''.join(chars)
 
         if draw_lines:
